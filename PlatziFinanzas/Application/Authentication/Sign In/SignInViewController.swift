73efab36cda8a2870da355bfc3057f64ca4bdd35
Andres Silva <andresilvagomez@gmail.com>
2018-12-10 10:06:07 -0500

    Desarollo Clase 7

--- PlatziFinanzas/Application/Authentication/Sign In/SignInViewController.swift
+++ PlatziFinanzas/Application/Authentication/Sign In/SignInViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class SignInViewController: UIViewController {
     @IBOutlet weak var emailTextField: UITextField!
@@ -19,23 +20,35 @@ class SignInViewController: UIViewController {
     }
     
     @IBAction func signIn(_ sender: Any) {
-        guard let email = emailTextField.text else {
-            return
+        guard let email = emailTextField.text,
+            validate(text: email, regex: "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}") else {
+                return
+        } 
+        
+        guard let password = passwordTextField.text,
+            validate(text: password, regex: "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[#$^+=!*()@%&]).{8,}$") else {
+                return
         }
         
-        let range = NSRange(location: 0, length: email.count)
-        let regex = try? NSRegularExpression(pattern: "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}", options: [])
-        let validation = regex?.firstMatch(in: email, options: [], range: range)
-        
-        if validation == nil {
-            return
+        Auth.auth().signIn(withEmail: email, password: password) { [weak self] (result, error) in
+            if let error = error {
+                let alert = UIAlertController(title: "Error", message: error.localizedDescription, preferredStyle: .alert)
+                let ok = UIAlertAction(title: "Ok", style: .default, handler: nil)
+                alert.addAction(ok)
+                self?.present(alert, animated: true, completion: nil)
+                return
+            }
+            
+            if result != nil {
+               self?.performSegue(withIdentifier: "goToMain", sender: self)
+            }
         }
-        
-        guard let password = passwordTextField.text else {
-            return
-        }
-        
-        print(email, password)
+    }
+    
+    private func validate(text: String, regex: String) -> Bool {
+        let range = NSRange(location: 0, length: text.count)
+        let regex = try? NSRegularExpression(pattern: regex)
+        return regex?.firstMatch(in: text, options: [], range: range) != nil
     }
     
     /*
