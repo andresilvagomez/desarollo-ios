d66c9ed581f01322af077310782aa6ebfc78dfc6
Andres Silva <andresilvagomez@gmail.com>
2018-12-10 10:22:24 -0500

    Desarollo Clase 8

--- PlatziFinanzas/Application/Authentication/Sign In/SignInViewController.swift
+++ PlatziFinanzas/Application/Authentication/Sign In/SignInViewController.swift
@@ -20,17 +20,12 @@ class SignInViewController: UIViewController {
     }
     
     @IBAction func signIn(_ sender: Any) {
-        guard let email = emailTextField.text,
-            validate(text: email, regex: "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}") else {
-                return
-        } 
         
-        guard let password = passwordTextField.text,
-            validate(text: password, regex: "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[#$^+=!*()@%&]).{8,}$") else {
-                return
-        }
-        
-        Auth.auth().signIn(withEmail: email, password: password) { [weak self] (result, error) in
+        SignInViewModel.signInWith(
+            email: emailTextField.text,
+            password: passwordTextField.text
+        ) { [weak self] (success, error) in
+                
             if let error = error {
                 let alert = UIAlertController(title: "Error", message: error.localizedDescription, preferredStyle: .alert)
                 let ok = UIAlertAction(title: "Ok", style: .default, handler: nil)
@@ -39,16 +34,10 @@ class SignInViewController: UIViewController {
                 return
             }
             
-            if result != nil {
-               self?.performSegue(withIdentifier: "goToMain", sender: self)
+            if success {
+                self?.performSegue(withIdentifier: "goToMain", sender: self)
             }
         }
-    }
-    
-    private func validate(text: String, regex: String) -> Bool {
-        let range = NSRange(location: 0, length: text.count)
-        let regex = try? NSRegularExpression(pattern: regex)
-        return regex?.firstMatch(in: text, options: [], range: range) != nil
     }
     
     /*
---
+++ PlatziFinanzas/Application/Authentication/Sign In/SignInViewModel.swift
@@ -0,0 +1,45 @@
+//
+//  SignInViewModel.swift
+//  PlatziFinanzas
+//
+//  Created by Andres Silva on 12/10/18.
+//  Copyright © 2018 Platzi. All rights reserved.
+//
+
+import Foundation
+import FirebaseAuth
+
+typealias SignInHandler = ( (_ success: Bool, _ error: Error?) -> Void )
+
+class SignInViewModel {
+
+    static func signInWith(email: String?, password: String?, handler: SignInHandler?) {
+        guard let email = email,
+            validate(text: email, regex: "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}") else {
+                return
+        }
+        
+        guard let password = password,
+            validate(text: password, regex: "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[#$^+=!*()@%&]).{8,}$") else {
+                return
+        }
+        
+        Auth.auth().signIn(withEmail: email, password: password) { (result, error) in
+            if let error = error {
+                handler?(false, error)
+            }
+            
+            if result != nil {
+                handler?(true, nil)
+            }
+        }
+        
+    }
+    
+    static private func validate(text: String, regex: String) -> Bool {
+        let range = NSRange(location: 0, length: text.count)
+        let regex = try? NSRegularExpression(pattern: regex)
+        return regex?.firstMatch(in: text, options: [], range: range) != nil
+    }
+    
+}
