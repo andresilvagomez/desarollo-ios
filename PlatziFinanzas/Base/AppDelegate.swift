7bfd06da8366e5690f53338fb70de535f58ab1dc
Andres Silva <andresilvagomez@gmail.com>
2018-12-10 14:43:32 -0500

    Desarollo Clase 14

--- PlatziFinanzas/Base/AppDelegate.swift
+++ PlatziFinanzas/Base/AppDelegate.swift
@@ -43,6 +43,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             name = "SignIn"
         }
         
+        let session = Auth.auth().currentUser != nil
+        
+        if session {
+            name = "Main"
+        }
+        
         let viewController = UIStoryboard(name: name, bundle: Bundle.main).instantiateInitialViewController()
         
         window?.rootViewController = viewController
